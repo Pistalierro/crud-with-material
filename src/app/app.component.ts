8c882f886202700bba0c7f6a76d64712b34acf33
Anatoliy Sysoienko <pistalierro@gmail.com>
2024-10-13 15:02:36 +0300

    создал таблицу с товарами загружаемую с сервера

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component, inject, OnInit} from '@angular/core';
+import {Component, inject, OnInit, ViewChild} from '@angular/core';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
@@ -6,6 +6,13 @@ import {MatDialog, MatDialogModule} from '@angular/material/dialog';
 import {DialogComponent} from './components/dialog/dialog.component';
 import {HttpService} from './services/http.service';
 import {ProductInterface} from './shared/types/product.interface';
+import {MatPaginator, MatPaginatorModule} from '@angular/material/paginator';
+import {MatTableDataSource, MatTableModule} from '@angular/material/table';
+import {MatSort, MatSortModule} from '@angular/material/sort';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {PRODUCT_COLUMNS} from './mock/table-data';
+import {CurrencyPipe, DatePipe} from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -14,18 +21,40 @@ import {ProductInterface} from './shared/types/product.interface';
     MatToolbarModule,
     MatButtonModule,
     MatIconModule,
-    MatDialogModule
+    MatDialogModule,
+    MatTableModule,
+    MatSortModule,
+    MatPaginatorModule,
+    MatFormFieldModule,
+    MatInputModule,
+    DatePipe,
+    CurrencyPipe
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent implements OnInit {
 
+  displayedColumns: string[] = PRODUCT_COLUMNS;
+  dataSource!: MatTableDataSource<any>;
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
   readonly dialog = inject(MatDialog);
   private httpService = inject(HttpService);
 
   ngOnInit(): void {
     this.getAllProducts();
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
   }
 
   createProduct() {
@@ -41,7 +70,9 @@ export class AppComponent implements OnInit {
   private getAllProducts(): void {
     this.httpService.readData().subscribe({
       next: (res: ProductInterface[]) => {
-        
+        this.dataSource = new MatTableDataSource(res);
+        this.dataSource.paginator = this.paginator;
+        this.dataSource.sort = this.sort;
       },
       error: err => console.log(err)
     });
---
+++ src/app/mock/table-data.ts
@@ -0,0 +1,1 @@
+export const PRODUCT_COLUMNS: string[] = ['name', 'category', 'price', 'productCondition', 'date', 'comment', 'actions'];
